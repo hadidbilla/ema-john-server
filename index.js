ad9b59bc360989b19d42c2d8e6bcf3727bd691bc
hbhridoy449 <50765387+hbhridoy449@users.noreply.github.com>
2021-03-28 19:08:59 +0600

    sever side project

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zta4c.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productsCollection = client.db("emaJhonStore").collection("products");
+  const ordersCollection = client.db("emaJhonStore").collection("orders");
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products).then((result) => {
+      res.send(result.insertedCount);
+    });
+  });
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+  app.get("/products", (req, res) => {
+    productsCollection
+      .find({})
+      .limit(20)
+      .toArray((err, document) => {
+        res.send(document);
+      });
+  });
+  app.get("/product/:key", (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+  app.post("/productsByKeys", (req, res) => {
+    const productsKeys = req.body;
+    productsCollection
+      .find({ key: { $in: productsKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+});
+
+app.listen(port);
